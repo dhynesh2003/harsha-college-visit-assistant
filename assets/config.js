ba5f8f93a990be738f6ac903472884b58d79a21c
dhynesh <ddhynesh@gmail.com>
2026-08-17 15:07:58 +0530

    Add OneSignal web push configuration

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,8 @@
-window.HARSHA_SUPABASE_CONFIG = {
+﻿window.HARSHA_SUPABASE_CONFIG = {
   url: "https://ncvklfodhnwbnwspztsz.supabase.co",
   anonKey: "sb_publishable_NA83ztBEAtb_hHvPTBcfUw_wcFxcOM7",
+  oneSignalAppId: "5eb1c9cc-b538-4a07-a8f0-f812b5ea1252",
   vapidPublicKey: "BLyUmxBugNw8kqGIYacY12FPRTdwuzP06oRf5sxiC0aIqNCh_m0cmJi1vIoOR_pn2vlJ83oMJ4kiGCY0YsKnrCw"
 };
+
+
